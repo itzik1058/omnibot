de992e2d416d99f1fa7fb348a8ec3d0b9675b40d
Itzik <itzik1058@gmail.com>
2023-09-15 12:45:51 +0300

    default data path

--- src/core/config.ts
+++ src/core/config.ts
@@ -5,9 +5,7 @@ import untildify from "untildify";
 const config = {
   clientId: process.env.CLIENT_ID || assert.fail("Missing CLIENT_ID"),
   token: process.env.DISCORD_TOKEN || assert.fail("Missing DISCORD_TOKEN"),
-  dataPath: untildify(
-    process.env.DATA_PATH || assert.fail("Missing DATA_PATH")
-  ),
+  dataPath: untildify(process.env.DATA_PATH ?? "/omnibot"),
   guildId: process.env.GUILD_ID || assert.fail("Missing GUILD_ID"),
   channelId: process.env.CHANNEL_ID || assert.fail("Missing CHANNEL_ID"),
 };
