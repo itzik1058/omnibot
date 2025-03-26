80b301d0033c2cc50fdadaf425a838124178f43f
Itzik <74200683+itzik1058@users.noreply.github.com>
2025-03-26 17:57:37 +0200

    log activity to csv

--- src/modules/activity.ts
+++ src/modules/activity.ts
@@ -1,3 +1,7 @@
+import { existsSync, type PathLike } from "node:fs";
+import { appendFile, writeFile } from "node:fs/promises";
+import path from "node:path";
+
 import { Collection, Events, GuildMember } from "discord.js";
 
 import config from "../config.ts";
@@ -9,18 +13,31 @@ type GuildMemberActivity = GuildMember & { activityStart: Date };
 
 export default class Activity extends OmnibotModule {
   private members: Collection<string, GuildMemberActivity>;
+  private logPath: PathLike;
   private task: ScheduledTask;
 
   constructor(omnibot: Omnibot) {
     super(omnibot);
 
     this.members = new Collection();
-    this.task = new ScheduledTask(this.tick, 1000);
+    this.logPath = path.join(config.dataPath, "activity.csv");
+    this.task = new ScheduledTask(this.tick.bind(this), 1000);
 
-    omnibot.client.once(Events.ClientReady, () => this.task.start());
+    omnibot.client.once(Events.ClientReady, () => {
+      this.onClientReady().catch((e) => console.error(e));
+    });
   }
 
-  private tick = () => {
+  private onClientReady = async () => {
+    if (!existsSync(this.logPath))
+      await writeFile(
+        this.logPath,
+        ["id", "username", "join", "leave", "nickname"].join(","),
+      );
+    this.task.start();
+  };
+
+  private async tick() {
     const guild = this.omnibot.client.guilds.cache.get(config.guildId);
     if (!guild) return;
     const members = guild.channels.cache
@@ -35,14 +52,21 @@ export default class Activity extends OmnibotModule {
       );
     const previousMembers = new Set(this.members.map((_, key) => key));
     const currentMembers = new Set(members.map((_, key) => key));
-    previousMembers.difference(currentMembers).forEach((key) => {
+    for (const key of previousMembers.difference(currentMembers)) {
       const member = this.members.get(key)!;
       const now = new Date();
-      console.log(
-        `Member ${member.user.username} was active`,
-        `from ${member.activityStart.toString()} to ${now.toString()}`,
+      await appendFile(
+        this.logPath,
+        "\n" +
+          [
+            member.id,
+            member.user.username,
+            member.activityStart.toISOString(),
+            now.toISOString(),
+            member.nickname,
+          ].join(","),
       );
-    });
+    }
     this.members = members;
-  };
+  }
 }
